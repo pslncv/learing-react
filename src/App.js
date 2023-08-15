8aec0b704dd19320ff06102ea99fd7f3a9977c63
pslncv <as.poselentsev@gmail.com>
2023-08-16 02:59:12 +0300

    v0.1 Init React App and Git, create Counter & ClassCounter components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ClassCounter from './components/ClassCounter';
+
+
+function App() {
+
+  return (
+    <div>
+      <ClassCounter />
+    </div>
+  );
+
+}
+
+export default App;
